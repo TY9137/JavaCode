4fe6c7757073fe1b27753f8696fa4bdd97868edb
TY <1041685201@qq.com>
2021-11-21 17:17:25 +0800

    first commit of 3 week

---
+++ Week3/src/main/java/HttpServerHelper/filter/HeaderHttpResponseFilter.java
@@ -0,0 +1,11 @@
+package HttpServerHelper.filter;
+
+import io.netty.handler.codec.http.FullHttpResponse;
+
+public class HeaderHttpResponseFilter implements HttpResponseFilter {
+    @Override
+    public void filter(FullHttpResponse response) {
+        response.headers().set("kk", "java-1-nio");
+    }
+}
+
---
+++ Week3/src/main/java/HttpServerHelper/filter/HttpResponseFilter.java
@@ -0,0 +1,9 @@
+package HttpServerHelper.filter;
+
+import io.netty.handler.codec.http.FullHttpResponse;
+
+public interface HttpResponseFilter {
+
+    void filter(FullHttpResponse response);
+
+}
